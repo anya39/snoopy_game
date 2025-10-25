752e56f9a040168e2d0f6d3e22f59bbf59adcba5
Anya Schenck <anya.schenck@gmail.com>
2025-10-25 16:27:10 -0400

    updated arrow movement and added window bounds for player

--- main.py
+++ main.py
@@ -9,16 +9,51 @@ pygame.display.set_caption("Snoopy Game")
 
 #player, snoopy
 player_image = pygame.image.load("snoopy_pumpkin.png")
-player_image = pygame.transform.smoothscale(player_image, (100,120))
+player_image = pygame.transform.smoothscale(player_image, (95,130))
 player_x = 350
 player_y = 480
 
-#game loop, keeps window open
+#player movement variables
+player_x_change = 0
+player_y_change = 0
+
+#game loop, also keeps window open
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    #check which keys have been pressed
+    keys = pygame.key.get_pressed()
+
+    #arrow key movement functionality when pressed
+    #horizontal movement
+    if keys[pygame.K_LEFT]:
+        player_x-=3
+    if keys[pygame.K_RIGHT]:
+        player_x+=3
+        
+    #vertical movement
+    if keys[pygame.K_UP]:
+        player_y-=3
+    if keys[pygame.K_DOWN]:
+        player_y+=3
+
+    #update player position
+    player_x += player_x_change
+    player_y += player_y_change
+
+    #keep player inside window bounds
+    if player_x < -20:
+        player_x = -20
+    elif player_x > 720:
+        player_x = 720
+    if player_y < -20:
+        player_y = -20
+    elif player_y > 490:
+        player_y = 490
+
     screen.blit(player_image, (player_x, player_y))
     pygame.display.update()
     
